3a77b5f0edd5abb0aef69066e85ec50d1de6a45f
jtjanecek <janecektyler@gmail.com>
2020-07-28 20:29:22 +0000

    Used wrong variable for passing serverip to docker

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 # Reset the docker image
 docker rmi dnas-test
 # Pass in the server IP
-docker build --rm --tag dnas-test --build-arg serverip=${server_ip} .
+docker build --rm --tag dnas-test --build-arg serverip=${1} .
